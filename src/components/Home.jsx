f50ce20b1d99731b7d10dbdb4819866d2698fcf7
RodolfoSouzaMenezes <rodolfosouzaofc@gmail.com>
2022-08-27 15:00:17 -0300

    refactor(home): added <br /> before the button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -25,6 +25,7 @@ function Home() {
               industry. Lorem Ipsum has been the industry's standard dummy text
               ever since the 1500s, when an unknown printer took a galley of
               type and scrambled it to make a type specimen book.
+              <br />
               <button onClick={sair}>Contact</button>
             </p>
           </div>
